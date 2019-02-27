93bc731064bace234c1ee2a04da728c53540fa6c
shinben0327 <shinben0327@gmail.com>
2019-02-27 14:27:38 +0900

    Modified so it keeps PEP#8

--- jsmaths.py
+++ jsmaths.py
@@ -8,6 +8,7 @@ This Python file will be used to store mathematical functions I made by myself.
 Type 'from jsmaths import X' to use the functions in other files. 
 """
 
+
 def factorial(x):
     number = x
     result = 1
@@ -16,23 +17,35 @@ def factorial(x):
         number -= 1
     return result
 
+
+def factorial(x):
+    if x == 1:
+        return 1
+    return x * factorial(x-1)
+
+
 def npr(n, r):
     return int(factorial(n)/factorial(n-r))
+
 
 def npir(n, r):
     return n ** r
 
+
 def ncr(n, r):
     return int(factorial(n)/factorial(n-r)/factorial(r))
 
+
 def nhr(n, r):
     return ncr(n+r-1, r)
+
 
 def mean(array):
     total = 0
     for i in array:
         total += i
     return total / len(array)
+
 
 def median(array):
     array.sort()
